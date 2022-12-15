ad8ddba806ca3f5bfbf7197c310df7f140f8193c
AtharvaKhule-12 <khuleatharva@gmail.com>
2022-12-15 21:03:19 +0530

    app build

---
+++ src/pages/index.js
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
+
+export default function Home({products}) {
+  return (
+    <div className="bg-gray-100">
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+
+      {/* Header */}
+      <Header />
+
+    <main className="max-w-screen-2xl mx-auto">
+      {/* Banner */}
+      <Banner />
+
+      {/* ProdcutFeed */}
+      <ProductFeed products={products}/>
+    </main>
+    </div>
+  );
+}
+
+export const getServerSideProps = async (context) => {
+  const products = await fetch('https://fakestoreapi.com/products').then(res => res.json());
+
+  return {
+    props: {
+      products: products,
+    },
+  };
+  
+}
